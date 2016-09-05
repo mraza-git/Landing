f6e0c011b30f700d12c88b90bd1836730072441f
ansaries <arslan@ansaries.com>
2016-09-05 16:59:30 +0400

    Version 0.1 Commit orignal
    Uploaded files

---
+++ src/app/main/sample/sample.module.js
@@ -0,0 +1,63 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.sample', [])
+        .config(config);
+
+    /** @ngInject */
+    function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider)
+    {
+        // State
+        $stateProvider
+            .state('app.sample', {
+                url    : '/sample',
+                views  : {
+                    'main@':{
+                        templateUrl: 'app/core/layouts/content-with-toolbar.html',                        
+                        controller: "MainController as vm"
+                    },
+                    'toolbar@app.sample':{
+                        templateUrl: 'app/toolbar/layouts/horizontal-navigation/toolbar.html',
+                        controller: "ToolbarController as vm"
+                    },
+                    'content@app.sample': {
+                        templateUrl: 'app/main/sample/sample.html',
+                        controller : 'SampleController as vm'
+                    }
+                },
+                resolve: {
+                    SampleData: function (msApi)
+                    {
+                        return msApi.resolve('sample@get');
+                    }
+                }
+                
+            });
+
+        // Translation
+        $translatePartialLoaderProvider.addPart('app/main/sample');
+
+        // Api
+        msApiProvider.register('sample', ['app/data/sample/sample.json']);
+
+        // Navigation
+        msNavigationServiceProvider.saveItem('fuse', {
+            title : 'SAMPLE',
+            group : true,
+            weight: 1
+        });
+
+        msNavigationServiceProvider.saveItem('fuse.sample', {
+            title    : 'Sample',
+            icon     : 'icon-tile-four',
+            state    : 'app.sample',
+            /*stateParams: {
+                'param1': 'page'
+             },*/
+            translate: 'SAMPLE.SAMPLE_NAV',
+            weight   : 1
+        });
+    }
+})();
